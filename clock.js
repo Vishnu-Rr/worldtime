5a5011fa7d90b04ff3c3289519c0d33adb6c5a76
vishnuraja005 <72242086+vishnuraja005@users.noreply.github.com>
2020-11-05 10:07:42 +0530

    Update clock.js

--- clock.js
+++ clock.js
@@ -69,7 +69,7 @@ time:"asia/calcutta"
          }
        ],
         selectedTimezone: 'Asia/calcutta',
-        weather:"chennai",
+        weather:"maharashtra",
         temper:'',
         des:'',
         sr:'',
